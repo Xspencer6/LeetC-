0cd2d4f3f06aad7a91bac080698592c2b08dc357
Spencer <spenceralmodiel@gmail.com>
2024-02-17 14:44:00 +0800

    Added new approach

--- 3_longest_substring__without_repeating_char.cpp
+++ 3_longest_substring__without_repeating_char.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-
+using namespace std;
 int lengthOfLongestSubstring(std::string s);
 
 int main(){
@@ -10,7 +10,7 @@ int main(){
     return 0;
 }
 
-int lengthOfLongestSubstring(std::string s){
+int lengthOfLongestSubstring(std::string s){        // this approach use hash-mapping to remember index values per occurrence of character
     if (s.size() == 0) return 0;
     int n = s.size();
     int best = 0;
@@ -36,3 +36,26 @@ int lengthOfLongestSubstring(std::string s){
     }
     return best;
 }
+/*
+This approach uses vector with values initialized as -1 to store index value of characters, if index[s[right]] >= left means it is a repeating char
+- same approach above : move left pointer the next position of last occurred character.
+- much simpler compared to solution above
+- always updates the value of index of character (to determine the position of left pointer if it encounters a repeating character but not in substring yet.)
+*/
+int longestSubString(string s){               
+    if (s.size() == 0) return 0;
+    int left = 0;
+    vector <int> index(128, -1);    // initialize a list of 128 length with -1 initialzied value.
+    int best = 0;
+
+    for (int right = 0; right < s.size(); right++){
+        if (index[s[right]] >= left){   // if index of right is greater than left : it is a repeating char
+            left = index[s[right]] + 1;  // move to the next char of last occurred
+            // compare with current best
+            best = max(best, right - left + 1);
+        }
+        index[s[right]] = right;    // update index of character whether unique or not
+        best = max(best, right - left + 1); // check for new best
+    }
+    return best;
+}  
