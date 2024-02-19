583ae5a8145d593a6bba01df1208aacaaa860a1b
Spencer <109234810+Xspencer6@users.noreply.github.com>
2024-02-19 23:48:19 +0800

    You are given two integer arrays nums1 and nums2,
    sorted in non-decreasing order, and two integers m and n,
    
    representing the number of elements in nums1 and nums2 respectively.

--- 53_merge_sorted_arr.cpp
+++ 53_merge_sorted_arr.cpp
@@ -6,7 +6,7 @@ void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n);
 int main(){
     std::vector <int> num1 = {1,2,0,0};
     std::vector <int> num2 = {3, 4};
-    merge(num1, 1, num2, 2);
+    merge(num1, 1, num2, 1);
 
     for (auto i : num1) std::cout << i << " ";
 
