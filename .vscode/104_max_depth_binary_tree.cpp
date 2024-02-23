c4f23d1aabd3da6c1c59db86d918524963a626c3
Spencer <spenceralmodiel@gmail.com>
2024-02-23 22:55:23 +0800

    Added iterative solution

--- .vscode/104_max_depth_binary_tree.cpp
+++ .vscode/104_max_depth_binary_tree.cpp
@@ -1,3 +1,5 @@
+#include <queue>
+
 struct TreeNode{
     int val;
     TreeNode* left;
@@ -7,13 +9,34 @@ struct TreeNode{
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
-int maxDepth(TreeNode* root){
+int maxDepth_recursion(TreeNode* root){
     if (root == nullptr) return 0;
     else{
-        int leftDepth = maxDepth(root->left);
-        int rightDepth = maxDepth(root->right);
+        int leftDepth = maxDepth_recursion(root->left);
+        int rightDepth = maxDepth_recursion(root->right);
 
         if (leftDepth > rightDepth) return (leftDepth + 1);
         else return rightDepth + 1;
     }
+}
+
+int maxDepth_iter(TreeNode* root){
+    int depth = 0;
+    std::queue <TreeNode*> queue;
+    queue.push(root);
+    queue.push(nullptr);
+    while (!queue.empty()){
+        TreeNode* temp = queue.front(); // get root
+        queue.pop();
+
+        if (temp == nullptr) depth++;   // means we finished 1 level
+
+        if (temp != nullptr){
+            if (temp->left) queue.push(temp->left);
+            if (temp->right) queue.push(temp->right);
+        }
+        else if (!queue.empty()) queue.push(nullptr);   // pushed null after pushing left and right to indicate we finished 1 level
+
+    }
+    return depth;
 }
