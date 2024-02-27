cf5c0c5ed179d0fa1fa0b44f9afae826bcc8d692
Spencer <spenceralmodiel@gmail.com>
2024-02-27 10:05:30 +0800

    Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
    A leaf is a node with no children.

---
+++ 112_path_sum.cpp
@@ -0,0 +1,8 @@
+#include "TreeNode.h"
+
+bool hasPathSum(TreeNode* root, int targetSum){
+    if (!root) return false;
+    if (root->val == targetSum && !root->left && !root->right) return true; // if the remaining sum is the value of current node and it is a leaf node 
+    // call left and right target - value
+    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+}
