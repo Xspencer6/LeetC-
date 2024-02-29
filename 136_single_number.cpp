cf82f208b61e5aa63dbefe2499dd8aa7f34ac57f
Spencer <spenceralmodiel@gmail.com>
2024-02-29 23:59:43 +0800

    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
    You must implement a solution with a linear runtime complexity and use only constant extra space.

---
+++ 136_single_number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int singleNumber(std::vector<int> &nums){
+    std::unordered_map <int, int> maps;
+    for (int i = 0; i < nums.size() - 1; i++){
+        maps[nums[i]] = i;
+        // check if last element
+        if (nums[i] == nums[nums.size() - 1]){
+            // check for duplicates
+            for (auto x : maps){
+                if (x.second == 1) return x.first
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    std::unordered_map <int, int> maps;
+    maps[1] = 0;
+    maps[2] = 1;
+    maps[1] = 2;
+
+    for (auto x : maps) std::cout << x.first << "&&" << x.second;
+
+    return 0;
+}
