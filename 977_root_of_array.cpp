7c0f3e7120675473e8f5c20273bd60c0ed899422
Spencer <109234810+Xspencer6@users.noreply.github.com>
2024-03-02 20:46:37 +0800

    Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted  in non-decreasing order.

---
+++ 977_root_of_array.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <cstdlib>
+
+std::vector<int> sortedSquares(std::vector<int> & nums){
+    std::vector<int> answer(nums.size());
+    // two pointer 
+    int left = 0, right = nums.size() - 1;
+    for (int i = nums.size() - 1; i >= 0; i--){
+        if(std::abs(nums[left]) > std::abs(nums[right])){
+            answer[i] = nums[left] * nums[left++];
+        }
+        else answer[i] = nums[right] * nums[right--];
+    }
+    return answer;
+}
