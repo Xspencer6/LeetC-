7d3842f1f04ed18296244877f715ae5cd115458e
Mj Spencer <spenceralmodiel@gmail.com>
2024-03-05 15:51:43 +0800

    up

---
+++ 4_median_sorted_array.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        std::vector<int> list;
+
+        for (auto num: nums1){
+            list.push_back(num);
+        }
+
+        for (auto num: nums2){
+            list.push_back(num);
+        }
+
+        std::sort()
+    }
